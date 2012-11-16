35c6fa32548d9443aa55104a32f25c796511afd2
Vladimir Fedorov <fedorovvs@gmail.com>
2012-11-16 16:38:45 -0500

    123

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello! Testing node.js.');
+  response.send('Hello! Testing node.js. 123.');
 });
 
 var port = process.env.PORT || 5000;
