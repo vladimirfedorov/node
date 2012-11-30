65774e13035125111a9ac961c9b0b69913c03179
Vladimir Fedorov <fedorovvs@gmail.com>
2012-12-01 01:20:29 +0600

    test

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var _ = require('underscore');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello! Testing node.js.\n');
+app.get('/', function(req, res) {
+	res.send('Hello! Testing node.js.\n');
+});
+
+app.get('/blog', function(req, res) {
+	res.send('Blog');
 });
 
 var port = process.env.PORT || 5000;
