6c6fed53ca1530301b254647f378c714b0444c90
Vladimir Fedorov <fedorovvs@gmail.com>
2012-12-17 15:05:05 +0600

    supports static pages

--- web.js
+++ web.js
@@ -1,17 +1,43 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var _ = require('underscore');
+var response;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-	res.send('Hello! Testing node.js.\n');
+	getPageContents("home", res);
 });
 
-app.get('/blog', function(req, res) {
-	res.send('Blog');
+app.get('/:page', function(req, res) {
+	getPageContents(req.params.page, res);
+});
+
+app.get('*', function(req, res) {
+	getPageContents("404", res);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function getPageContents(name, res) {
+	response = res;
+	if (path.existsSync("pages/"+name+".html")) {
+		fs.readFile("pages/"+name+".html", "binary", function(e, content) {
+			if (e) throw e;
+			response.send(content);;
+		});
+		
+		//return "Page: /pages/" + name + ".html";
+	}
+	else
+	{
+		fs.readFile("pages/404.html", "binary", function(e, content) {
+			if (e) throw e;
+			response.send(content);
+		});
+	}
+}
